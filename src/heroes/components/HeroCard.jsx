97e16566feca88f9ac1a269ad21f1fa761cf80ba
JL-OLEMAR <joseluis19963@gmail.com>
2022-07-05 23:18:39 -0500

    fix: Show imgs correctly

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -12,7 +12,7 @@ export function HeroCard({
   first_appearance,
   characters
 }) {
-  const heroImageUrl = `./assets/heroes/${id}.jpg`
+  const heroImageUrl = `/assets/heroes/${id}.jpg`
 
   return (
     <div className='col animate__animated animate__fadeIn'>
